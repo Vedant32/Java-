45c78b1220c314da3ad2af554aa12d1b1a9fb20b
Vedantthakar <Vedantthakar32@gmail.com>
2022-06-16 15:42:37 -0400

    This is project

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author vedan
+ */
+
+public class Mavenproject1{
+  public static void main(String[] args) {
+
+    // take two numbers
+    double num1 = 10;
+    double num2 = 20;
+
+    // declare sum variable
+    // and initialize with 0
+    double sum = 0.0;
+    // declare average variable
+    double avg = 0.0;
+
+    // calculate the sum value
+    sum = num1 + num2;
+    // calculate the average value
+    avg = sum/2;
+
+    // display result
+    System.out.println("Average: " + avg );
+  }
+}
